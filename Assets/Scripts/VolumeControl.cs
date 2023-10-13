c5fa4dc109fab6fae5fb590ff38b125eccf3c7d2
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2023-10-13 10:29:36 +0100

    PlayerPrefs
    PlayerPrefs added for options menu (also made saveloadmanager persistant) - having issue with linkage between audio mixer and sliders - doesnt appear to be able to find exposed values from audio mixer - but base functionality implemented.

--- Assets/Scripts/VolumeControl.cs
+++ Assets/Scripts/VolumeControl.cs
@@ -4,19 +4,26 @@ using UnityEngine.Audio;
 public class VolumeControl : MonoBehaviour
 {
     public AudioMixer audioMixer;  // Reference to the Audio Mixer asset
+    public string masterVolumeParameterName = "Master";
+    public string musicVolumeParameterName = "Music";
+    public string sfxVolumeParameterName = "SFX";
+
 
     public void SetMasterVolume(float volume)
     {
-        audioMixer.SetFloat("MasterVolume", volume);
+        Debug.Log("Setting Master Volume to " + volume);
+        audioMixer.SetFloat(masterVolumeParameterName, volume);
     }
 
     public void SetMusicVolume(float volume)
     {
-        audioMixer.SetFloat("MusicVolume", volume);
+        Debug.Log("Setting Music Volume to " + volume);
+        audioMixer.SetFloat(musicVolumeParameterName, volume);
     }
 
     public void SetSFXVolume(float volume)
     {
-        audioMixer.SetFloat("SFXVolume", volume);
+        Debug.Log("Setting SFX Volume to " + volume);
+        audioMixer.SetFloat(sfxVolumeParameterName, volume);
     }
 }
