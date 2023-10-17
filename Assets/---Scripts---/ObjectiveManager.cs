861f28bf464616b4fbe52d63989776c39392cb62
Adam-Bakerr <s225170@uos.ac.uk>
2023-10-17 09:11:58 +0100

    Revert "Revert "altered var names""
    This reverts commit 4be5ff94148632e15cf01660238ef3522aed760b.

--- Assets/---Scripts---/ObjectiveManager.cs
+++ Assets/---Scripts---/ObjectiveManager.cs
@@ -1,42 +1,51 @@
-using System.Collections;
+using JetBrains.Annotations;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ObjectiveManager : MonoBehaviour
 {
-    [SerializeField] public static int ObjectiveCount = 4;
-    [SerializeField] List<int> ActiveObjectIDs;
+    //The amount of tasks we wish to select this run
+    public static int ObjectiveCount = 4;
 
+    [SerializeField] private List<int> _activeObjectIDs;
+
+    //Unity events
     public static UnityEvent<int> ObjectiveComplete = new UnityEvent<int>();
-    public static UnityEvent AllObjectivesComplete = new UnityEvent();
-    static Dictionary<int, Objective> ObjectivesInLevel { set; get; } = new Dictionary<int, Objective>();
 
-    void Start()
+    public static UnityEvent AllObjectivesComplete = new UnityEvent();
+
+    //Dict of all tasks in the level sorted by their IDs
+    private static Dictionary<int, Objective> ObjectivesInLevel { set; get; } = new Dictionary<int, Objective>();
+
+    private void Start()
     {
-        ActiveObjectIDs = GetRandomTaskIDs(ObjectiveCount);
+        _activeObjectIDs = GetRandomTaskIDs(ObjectiveCount);
         ObjectiveComplete.AddListener(OnObjectiveComplete);
     }
 
-    void OnObjectiveComplete(int id)
+    //called when a single task has been completed via a public event
+    private void OnObjectiveComplete(int id)
     {
-        ActiveObjectIDs.Remove(id);
-        if(CheckIfAllTasksCompleted())OnAllTasksCompleted();
+        _activeObjectIDs.Remove(id);
+        if (CheckIfAllTasksCompleted()) OnAllTasksCompleted();
     }
 
-    bool CheckIfAllTasksCompleted()
+    //Check if all tasks have been completed
+    private bool CheckIfAllTasksCompleted()
     {
-        if (ActiveObjectIDs.Count <= 0) return true;
+        if (_activeObjectIDs.Count <= 0) return true;
         return false;
     }
 
-    void OnAllTasksCompleted()
+    //Code here for when all tasks have been finished
+    private void OnAllTasksCompleted()
     {
         AllObjectivesComplete?.Invoke();
     }
 
+    //Called by each individual objective when it is created so its registered before we pick all tasks
     public static int? AddObjective(Objective objectiveToAdd)
     {
         if (ObjectivesInLevel.ContainsValue(objectiveToAdd)) return null;
@@ -45,28 +54,29 @@ public class ObjectiveManager : MonoBehaviour
         return id;
     }
 
-    
-    [CanBeNull]public static Objective GetObjectiveById(int id)
+    //Gets the task by its associated id
+    [CanBeNull]
+    public static Objective GetObjectiveById(int id)
     {
         if (ObjectivesInLevel.TryGetValue(id, out Objective valueToReturn)) return valueToReturn;
         return null;
     }
 
-
+    //Gets a random list of tasks defined within the count, that can only select a single task once
     public static List<int> GetRandomTaskIDs(int count)
     {
-        List<int> allAvalibleIds = ObjectivesInLevel.Keys.ToList();
-        List<int> ReturnValues = new List<int>();
+        List<int> allAvailableIds = ObjectivesInLevel.Keys.ToList();
+        List<int> returnValues = new List<int>();
 
-        count = Mathf.Min(count, allAvalibleIds.Count);
+        count = Mathf.Min(count, allAvailableIds.Count);
 
         for (int i = 0; i < count; i++)
         {
-            int Rand = Random.Range(0, allAvalibleIds.Count - 1);
-            ReturnValues.Add(allAvalibleIds[Rand]);
-            allAvalibleIds.RemoveAt(Rand);
+            int Rand = Random.Range(0, allAvailableIds.Count - 1);
+            returnValues.Add(allAvailableIds[Rand]);
+            allAvailableIds.RemoveAt(Rand);
         }
 
-        return ReturnValues;
+        return returnValues;
     }
 }
