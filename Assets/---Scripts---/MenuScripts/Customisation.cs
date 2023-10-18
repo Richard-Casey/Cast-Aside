4710d34e582cfb911ea21c06777decef2df8f3ee
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2023-10-18 15:29:03 +0100

    Customisation Menu
    Implemented - need preview pics for each design.

--- Assets/---Scripts---/MenuScripts/Customisation.cs
+++ Assets/---Scripts---/MenuScripts/Customisation.cs
@@ -13,10 +13,11 @@ public class Customisation : MonoBehaviour
     {
         // Load all the player prefabs from the Resources/PlayerPrefabs folder into the array
         playerPrefabs = Resources.LoadAll<GameObject>("PlayerPrefabs");
-
+        Debug.Log("Loaded prefabs: " + string.Join(", ", System.Array.ConvertAll(playerPrefabs, prefab => prefab.name)));
         // Instantiate the initial player prefab
         currentPlayer = Instantiate(playerPrefabs[currentPlayerIndex], playerParent.position, Quaternion.identity, playerParent);
     }
+
 
     void Update()
     {
@@ -73,6 +74,24 @@ public class Customisation : MonoBehaviour
         WaypointRunner waypointRunner = currentPlayer.GetComponent<WaypointRunner>();
         waypointRunner.SetInitialState(target, isPaused);
     }
+
+    public void SelectPrefab(int index)
+    {
+        Debug.Log("SelectPrefab called with index: " + index);
+        // Store the selected prefab index for later use
+        currentPlayerIndex = index;
+    }
+
+
+    public void ConfirmSelection()
+    {
+        Debug.Log("ConfirmSelection called. Current index: " + currentPlayerIndex);
+        // Instantiate the selected prefab
+        Destroy(currentPlayer);
+        currentPlayer = Instantiate(playerPrefabs[currentPlayerIndex], playerParent.position, Quaternion.identity, playerParent);
+    }
+
+
 
 
 
--- Assets/---Scripts---/MenuScripts/MenuButtons.cs
+++ Assets/---Scripts---/MenuScripts/MenuButtons.cs
@@ -7,6 +7,7 @@ public class MenuButtons : MonoBehaviour
 
     public GameObject optionsMenu;
     public GameObject mainMenu;
+    public GameObject customisationMenu;
 
 
     public void StartButton()
@@ -31,5 +32,17 @@ public class MenuButtons : MonoBehaviour
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+    }
+
+    public void OpenCustomisationMenu()
+    {
+        mainMenu.SetActive(false);
+        customisationMenu.SetActive(true);
+    }
+
+    public void CloseCustomisationMenu()
+    {
+        mainMenu.SetActive(true);
+        customisationMenu.SetActive(false);
     }
 }
