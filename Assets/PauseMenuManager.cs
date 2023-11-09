38a86a2d851f0b11eadd113e6fd9beebdbed5f48
Adam-Bakerr <s225170@uos.ac.uk>
2023-11-09 08:39:58 +0000

    Changed Pause Menu To Work

--- Assets/PauseMenuManager.cs
+++ Assets/PauseMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -14,10 +15,74 @@ public class PauseMenuManager : MonoBehaviour
     [SerializeField] GameObject MainMenu;
     [SerializeField] GameObject OptionsMenu;
     [SerializeField] GameObject ConfirmQuit;
+
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider Master;
+    [SerializeField] Slider SFX;
+    [SerializeField] Slider Music;
     // Start is called before the first frame update
     public void Start()
     {
         InputManager.onPausePress.AddListener(onPause);
+
+        Master.onValueChanged.AddListener(SetMasterVolume);
+        SFX.onValueChanged.AddListener(SetMusicVolume);
+        Music.onValueChanged.AddListener(SetSFXVolume);
+
+    }
+
+    public void OnDisable()
+    {
+        InputManager.onPausePress.RemoveListener(onPause);
+        Master.onValueChanged.RemoveListener(SetMasterVolume);
+        SFX.onValueChanged.RemoveListener(SetMusicVolume);
+        Music.onValueChanged.RemoveListener(SetSFXVolume);
+
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        Debug.Log($"[AudioManager] Setting Master Volume: {volume}");
+        if (volume == 0)
+        {
+            mixer.SetFloat("MasterVolume", -80); // Mute the volume
+        }
+        else
+        {
+            mixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        }
+        PlayerPrefs.SetFloat(PlayerPrefsKeys.MasterVolume, volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        Debug.Log($"[AudioManager] Setting Music Volume: {volume}");
+        if (volume == 0)
+        {
+            mixer.SetFloat("MusicVolume", -80); // Mute the volume
+        }
+        else
+        {
+            mixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+        }
+        PlayerPrefs.SetFloat(PlayerPrefsKeys.MusicVolume, volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        Debug.Log($"[AudioManager] Setting SFX Volume: {volume}");
+        if (volume == 0)
+        {
+            mixer.SetFloat("SFXVolume", -80); // Mute the volume
+        }
+        else
+        {
+            mixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+        }
+        PlayerPrefs.SetFloat(PlayerPrefsKeys.SFXVolume, volume);
+        PlayerPrefs.Save();
     }
 
 
@@ -31,7 +96,16 @@ public class PauseMenuManager : MonoBehaviour
         mainMenuRectTransform.transform.DOLocalMove(InitalPos, 1f);
         */
 
+        /*
+        mainMenuRectTransform.anchoredPosition = mainMenuRectTransform.anchoredPosition + (Vector2.up * Screen.currentResolution.height);
+
+        DOTween.To(() => mainMenuRectTransform.anchoredPosition, x => mainMenuRectTransform.anchoredPosition = x, mainMenuRectTransform.anchoredPosition - (Vector2.up * Screen.currentResolution.height), 2);
+        */
+
     }
+
+
+
 
     public void ReturnToMenu()
     {
